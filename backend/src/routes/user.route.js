6ad1aa27460631ea25b794a44fba69d55f3a79b4
cryptasim <asimkumarhansda@gmail.com>
2025-07-12 19:48:03 +0530

    Created Send Friend Request Endpoint

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getRecommendedUsers, getMyFriends } from "../controllers/user.controller.js";
+import { getRecommendedUsers, getMyFriends, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router();
 // apply auth middleware to all routes
 router.use(protectRoute);
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
+router.post("/friend-request/:id", sendFriendRequest);
 export default router;
